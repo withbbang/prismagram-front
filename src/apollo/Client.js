ecdd2464a03af8bc6ef17b4f934a69e22c7dec1b
Bread <withbbang@gmail.com>
2020-07-30 22:22:45 +0900

    commit

--- src/apollo/Client.js
+++ src/apollo/Client.js
@@ -4,7 +4,7 @@ import { defaults, resolvers } from "./LocalState";
 export default new ApolloClient({
   uri:
     process.env.NODE_ENV === "development"
-      ? "http://localhost:4000"
+      ? "https://e634c8ea4e42.ngrok.io"
       : "https://youngstagram-backend.herokuapp.com/",
   clientState: {
     defaults,
