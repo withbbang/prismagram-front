9066ffa2cb34502159c88e1aa25841976675d480
Bread <withbbang@gmail.com>
2020-07-30 22:57:36 +0900

    commit

--- src/apollo/Client.js
+++ src/apollo/Client.js
@@ -2,10 +2,10 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri:
-    process.env.NODE_ENV === "development"
-      ? "https://e634c8ea4e42.ngrok.io"
-      : "https://youngstagram-backend.herokuapp.com/",
+  uri: "https://e634c8ea4e42.ngrok.io",
+  // process.env.NODE_ENV === "development"
+  //   ? "https://e634c8ea4e42.ngrok.io"
+  //   : "https://youngstagram-backend.herokuapp.com/",
   clientState: {
     defaults,
     resolvers,
