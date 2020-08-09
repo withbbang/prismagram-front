ffdb51179215504efeb83c1253d1ffe8d7aa1e8c
Bread <withbbang@gmail.com>
2020-08-09 16:33:42 +0900

    commit

--- src/apollo/Client.js
+++ src/apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "https://4f13fb3fbe6a.ngrok.io",
+  uri: "https://b37a35da1775.ngrok.io",
   // uri:
   //   process.env.NODE_ENV === "development"
   //     ? "http://localhost:4000"
