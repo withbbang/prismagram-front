82c2f636c793a959c532f06b607325bf8b71e5c1
Bread <withbbang@gmail.com>
2020-12-02 08:36:56 +0900

    commit for changing uri

--- src/apollo/Client.js
+++ src/apollo/Client.js
@@ -2,7 +2,7 @@ import ApolloClient from "apollo-boost";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  uri: "https://1ca391df3388.ngrok.io",
+  uri: "https://7fd84dfe5b1a.ngrok.io",
   // uri:
   //   process.env.NODE_ENV === "development"
   //     ? "http://localhost:4000"
